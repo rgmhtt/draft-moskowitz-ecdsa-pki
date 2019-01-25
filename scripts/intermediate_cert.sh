b5237d9a31b8090b66fad7c409be07c1bbff0c05
Michael Richardson <mcr@sandelman.ca>
2019-01-25 16:55:36 -0500

    fix two references to openssl-root.cnf, and remove mandatory match on countryName

--- scripts/intermediate_cert.sh
+++ scripts/intermediate_cert.sh
@@ -14,7 +14,7 @@ fi
 # Create the CSR
 
 echo GENERATING and SIGNING REQ intermediate
-openssl req -config $cadir/openssl-root.cnf $passin \
+openssl req -config $cfgdir/openssl-root.cnf $passin \
     -key $intdir/private/intermediate.key.$format -batch \
     -keyform $format -outform $format -subj "$DN" -new -sha256\
     -out $intdir/csr/intermediate.csr.$format
@@ -28,14 +28,13 @@ openssl rand -hex $sn > $intdir/serial # hex 8 is minimum, 19 is maximum
 
 if [ ! -f $cadir/certs/intermediate.cert.pem ]; then
     # Note 'openssl ca' does not support DER format
-    openssl ca -config $cadir/openssl-root.cnf -days 3650 $passin \
+    openssl ca -config $cfgdir/openssl-root.cnf -days 3650 $passin \
             -extensions v3_intermediate_ca -notext -md sha256 -batch \
             -in $intdir/csr/intermediate.csr.$format\
             -out $cadir/certs/intermediate.cert.pem
     chmod 444 $cadir/certs/intermediate.cert.$format
     rm -f $cadir/certs/ca-chain.cert.$format
 fi
-
 
 openssl verify -CAfile $cadir/certs/ca.cert.$format\
      $cadir/certs/intermediate.cert.$format
